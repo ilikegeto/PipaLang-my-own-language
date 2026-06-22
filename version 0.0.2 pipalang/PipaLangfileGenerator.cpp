a7c36b456668638fd59211e97acbc6792f3e4dac
ilikegeto <rafalexa63@gmail.com>
2026-06-22 17:05:10 +0700

    Enhance main function with usage instructions
    Added comments to guide users on how to run the compiled executable.

--- version 0.0.2 pipalang/PipaLangfileGenerator.cpp
+++ version 0.0.2 pipalang/PipaLangfileGenerator.cpp
@@ -5,5 +5,11 @@
 int main() {
     PipaCompiler PipaCompiler;
     PipaCompiler.PipaToExe("pipalang.pipa", "piplang"); //<- ("your .pipa file name", "your pipa name .exe (customizable)")
+    //how to run it?vvv
+    //after you write your .pipa file and your customizable .exe name
+    //write first download code runner
+    //after you download it run this file
+    //after this file finish compiling
+    //write .\your .exe name
     return 0;
 }
